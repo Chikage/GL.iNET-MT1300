f2e10533214177c90234b1751d585424389971f7
Shiryee Lin <908087650@QQ.com>
2021-03-30 12:45:43 +0800

    Initial commit

---
+++ scripts/lean.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+pushd package/lean
+git clone --depth=1 https://github.com/fw876/helloworld
+#sed -i 's/#src-git helloworld/src-git helloworld/g' ./feeds.conf.default
+rm -rf luci-theme-argon
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git
+popd
+
+mkdir package/community
+pushd package/community
+git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall
+popd
+
+#sed -i 's/192.168/10.168/g' package/base-files/files/bin/config_generate
