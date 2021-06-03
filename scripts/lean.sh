509289cadf18d7144187446e8749b92bff144c58
Shiryee Lin <Chikage@live.cn>
2021-06-03 18:02:01 +0800

    add luci-app-argon-config

--- scripts/lean.sh
+++ scripts/lean.sh
@@ -4,7 +4,8 @@ pushd package/lean
 git clone --depth=1 https://github.com/fw876/helloworld
 #sed -i 's/#src-git helloworld/src-git helloworld/g' ./feeds.conf.default
 rm -rf luci-theme-argon
-git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git
+git clone -b master https://github.com/jerrykuku/luci-theme-argon.git
+git clone -b master https://github.com/jerrykuku/luci-app-argon-config.git
 popd
 
 mkdir package/community
